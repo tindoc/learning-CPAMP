74e5aa2564970f91b6da4ad89f51b8dfb928edb6
Tindoc <qinghuang1995@gmail.com>
2016-09-20 00:56:10 +0800

    it can run now
    1.add head <stdbool,h>
    2,change the function "are_anagrams", first time I misunderstand the require
    3.it can run now

--- Chapter7/Chapter13-P14.c
+++ Chapter7/Chapter13-P14.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_LEN 50
 
@@ -8,34 +9,42 @@ bool are_anagrams(const char *word1, const char *word2);
 int main(void)
 {
 	char first_word[MAX_LEN + 1], second_word[MAX_LEN + 1];
-	
+
 	printf("Enter first word: ");
 	gets(first_word);
 	printf("Enter second word: ");
 	gets(second_word);
-	
+
 	if (are_anagrams(first_word, second_word))
 		printf("The words are anagrams");
 	else
 		printf("The words are not anagrams");
-	
+
 	return 0;
 }
 
 bool are_anagrams(const char *word1, const char *word2)
 {
-	int len1, len2;
-	int i;
-	
+	int len1, len2, alphabet[26]={0}, i, site1, site2;
+
 	len1 = strlen(word1);
 	len2 = strlen(word2);
-	
+
 	if (len1 != len2)
 		return false;
-	
-	for (i = 0; i < len1; i++)
-		if (word1[i] != word2[len1 - i])
-			return false;
-	
+
+	for (i = 0; i < len1; i++) {
+        site1 = word1[i] - 'a';
+        alphabet[site1]++;
+	}
+    for (i = 0; i < len2; i++) {
+        site2 = word2[i] - 'a';
+        alphabet[site2]--;
+    }
+
+    for (i = 0; i < len1; i++)
+        if (alphabet[i] != 0)
+            return false;
+
 	return true;
-}	
+}
